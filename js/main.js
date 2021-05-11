10ca976c1d2a6a2cccd0be7b0330d37b5cd052da
sona-juhasova <sonajuhasova.dev@gmail.com>
2021-05-11 22:13:09 +0200

    products rendered

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // /* fetch data from catalogue-one JSON */
 
 function loadCatalogue() {
-    fetch('../json/catalogue-one.json')
+    fetch('./json/catalogue-one.json')
         .then(function (response) {
             return response.json();
         })
@@ -17,35 +17,20 @@ function appendCatalogue(sections) {
     let htmlTemplate = "";
     for (let section of sections) {
         if (section.type === "intro") {
-            htmlTemplate += introTemplate ;
+            htmlTemplate += introTemplate(section);
 
         }
 
         if (section.type === "text") {
-            htmlTemplate += /*html*/ `
-            <div class="text-element">
-                <h1>${section.title}</h1>
-                <h2>${section.subtitle}</h2>
-                <h3>${section.article}</h3>
-            </div>
-            `;
+            htmlTemplate += textElementTemplate(section);
         }
 
 
         if (section.type === "category") {
-            for (let product of section.products) {
+            if (section.products.length == 20) {
+ 
+                htmlTemplate += sectionLarge(section);
 
-
-                htmlTemplate += /*html*/ ` 
-        <div class="product-container" style="background-image:url('${product.img_url[0]}');">
-            <div class="action-btn" style="background-color: ${settings.font};"></div>
-            <div class="product-details">
-                <h1>${product.title}</h1>
-                <h2>${product.price}</h2>
-                <p>${product.description}</p>
-            </div>
-        </div>
-        `;
             }
         }
 
@@ -55,15 +40,15 @@ function appendCatalogue(sections) {
 
 function openPopup(target) {
     var popupWrapper = target.parentElement.getElementsByClassName("popup-wrapper")[0];
-   
-        popupWrapper.style.display = "block"; 
-    }
+
+    popupWrapper.style.display = "block";
+}
 
 
 function closePopup(target) {
     var popupWrapper = target.closest(".popup-wrapper");
-        popupWrapper.style.display = "none";
-    
+    popupWrapper.style.display = "none";
+
 }
 
 loadCatalogue();
--- js/templates.js
+++ js/templates.js
@@ -1,36 +1,44 @@
-let introTemplate = /*html*/ `
+
+
+let introTemplate = function (section)
+{
+    return /*html*/ `
 <div class="intro-img">
 <img src="${section.intro_img}">
 </div>
-
 `;
+}
 
-let textElementTemplate = /*html*/ `
+let textElementTemplate =  function (section)
+{
+    return /*html*/ `
 <div class="text-element">
     <h1>${section.title}</h1>
     <h2>${section.subtitle}</h2>
     <h3>${section.article}</h3>
 </div>
 `;
+}
 
-let singleProductTemplate = /*html*/ `
-<div class="product-container" style="background-image:url('json/media-cat-one/s11.png');">
+// product templates
+
+let singleProductTemplate =  function (product)
+{
+    return /*html*/ `
+<div class="product-container" style="background-image:url('${product.img_url[0]}');">
             <div class="action-btn" style="background-color: red;" onclick="openPopup(this)"></div>
             <div class="product-details">
                 <h1>${product.title}</h1>
                 <h2>${product.price} kr</h2>
                 <p>${product.description}</p>
             </div>
-
             <!-- popup -->
             <div class="popup-wrapper" style="display: none;">
                 <div class="popup">
                     <div class="slider-wrapper">
                         <img class="left" src="media/arrow-left.svg" />
                         <img class="right" src="media/arrow-right.svg" />
-
                         <div class="images"></div>
-
                         <div class="dots-wrapper" style="text-align:center">
                             <span class="dot" onclick="setSliderImage(0)"></span>
                             <span class="dot" onclick="setSliderImage(1)"></span>
@@ -43,8 +51,6 @@ let singleProductTemplate = /*html*/ `
                         <h1>${product.title}</h1>
                         <h2>${product.price} kr</h2>
                         <p>${product.description}</p>
-
-
                         <select name="dropdown" id="select">
                             <option value="">Select</option>
                             <option value="option">${product.variant}</option>
@@ -56,69 +62,114 @@ let singleProductTemplate = /*html*/ `
             </div>
         </div>
         `;
-
-let parallaxTemplate = /*html*/ `
-<div class="parallax-window" data-parallax="scroll" data-image-src="json/media-cat-one/s11.png"></div>
+}
+let parallaxTemplate =  function (section)
+{
+    return /*html*/ `
+<div class="parallax-window" data-parallax="scroll" data-image-src="json/media-cat-one/t1a.jpeg"></div>
         `;
-
-let slideshowTemplate = /*html*/ `
+}
+let slideshowTemplate =  function (product1,product2,product3)
+{
+    return /*html*/ `
 <div class="container">
             <div class="mySlides">
-                <div class="product-container-slideshow" style="background-image:url('json/media-cat-one/s1.png');">
+                <div class="product-container-slideshow" style="background-image:url('${product1.img_url[0]}');">
                     <div class="action-btn" style="background-color: red;"></div>
                     <div class="product-details-slideshow">
-                        <h1>${product.title}</h1>
-                        <h2>${product.price} kr</h2>
-                        <p>${product.description}</p>
+                        <h1>${product1.title}</h1>
+                        <h2>${product1.price} kr</h2>
+                        <p>${product1.description}</p>
                     </div>
                 </div>
             </div>
-
             <div class="mySlides">
-                <div class="product-container-slideshow" style="background-image:url('json/media-cat-one/s11.png');">
+                <div class="product-container-slideshow" style="background-image:url('${product2.img_url[0]}');">
                     <div class="action-btn" style="background-color: red;"></div>
                     <div class="product-details-slideshow">
-                        <h1>${product.title}</h1>
-                        <h2>${product.price} kr</h2>
-                        <p>${product.description}</p>
+                        <h1>${product2.title}</h1>
+                        <h2>${product2.price} kr</h2>
+                        <p>${product2.description}</p>
                     </div>
                 </div>
             </div>
-
             <div class="mySlides">
-                <div class="product-container-slideshow" style="background-image:url('json/media-cat-one/s11.png');">
+                <div class="product-container-slideshow" style="background-image:url('${product3.img_url[0]}');">
                     <div class="action-btn" style="background-color: red;"></div>
                     <div class="product-details-slideshow">
-                        <h1>${product.title}</h1>
-                        <h2>${product.price} kr</h2>
-                        <p>${product.description}</p>
+                        <h1>${product3.title}</h1>
+                        <h2>${product3.price} kr</h2>
+                        <p>${product3.description}</p>
                     </div>
                 </div>
             </div>
-
-
             <a class="prev" onclick="plusSlides(-1)">
                 <img class="left" src="media/arrow-up.svg" />
             </a>
             <a class="next" onclick="plusSlides(1)">
                 <img class="left" src="media/arrow-down.svg" />
             </a>
-
-
-
             <div class="row">
                 <div class="column">
-                    <img class="demo" src="json/media-cat-one/s1.png" onclick="currentSlide(1)">
+                    <img class="demo" src="${product1.img_url[0]}" onclick="currentSlide(1)">
                 </div>
                 <div class="column">
-                    <img class="demo" src="json/media-cat-one/s11.png" onclick="currentSlide(2)">
+                    <img class="demo" src="${product2.img_url[0]}" onclick="currentSlide(2)">
                 </div>
                 <div class="column">
-                    <img class="demo" src="json/media-cat-one/s11.png" onclick="currentSlide(3)">
+                    <img class="demo" src="${product3.img_url[0]}" onclick="currentSlide(3)">
                 </div>
-
             </div>
         </div>
         `;
 
+}
+
+
+// section templates
+
+let sectionLarge = function (section)
+{
+    return /*html*/ `
+    <h1 class="section-title" style="font-family:'Times New Roman', Times, serif">${section.title}</h1>
+
+    <div class="section-container-large">
+
+        <div class="section-item-slider">${slideshowTemplate(section.products[0],section.products[1],section.products[2])}</div>
+
+        <div class="section-item-small">${singleProductTemplate(section.products[3])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[4])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[5])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[6])}</div>
+
+        <div class="group-of-five">
+            <div>
+                <div class="section-item-small">${singleProductTemplate(section.products[7])}</div>
+                <div class="section-item-small">${singleProductTemplate(section.products[8])}</div>
+            </div>
+            <div class="section-item-big">${singleProductTemplate(section.products[9])}</div>
+            <div>
+                <div class="section-item-small">${singleProductTemplate(section.products[10])}</div>
+                <div class="section-item-small">${singleProductTemplate(section.products[11])}</div>
+            </div>
+        </div>
+
+        <div class="section-item-small">${singleProductTemplate(section.products[12])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[13])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[14])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[15])}</div>
+
+        <div class="section-item-parallax">${parallaxTemplate(section)}</div> 
+        
+        <div class="section-item-small">${singleProductTemplate(section.products[16])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[17])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[18])}</div>
+        <div class="section-item-small">${singleProductTemplate(section.products[19])}</div>
+
+    </div>
+    
+    
+    `
+
+}
 
